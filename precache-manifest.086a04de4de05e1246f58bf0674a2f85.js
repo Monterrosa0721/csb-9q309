4d9999b23b720fc43f0e005959436d56c2351b29
Code Sandbox <hello@codesandbox.io>
2021-09-21 01:53:11 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.086a04de4de05e1246f58bf0674a2f85.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7386a1435cadb9358fa85401e9f3b3bd",
+    "url": "https://monterrosa0721.github.io/csb-9q309/index.html"
+  },
+  {
+    "revision": "f5acea20083d35f3b510",
+    "url": "https://monterrosa0721.github.io/csb-9q309/static/css/main.afab036c.chunk.css"
+  },
+  {
+    "revision": "8d9beec715d77db91f72",
+    "url": "https://monterrosa0721.github.io/csb-9q309/static/js/2.37ef8c98.chunk.js"
+  },
+  {
+    "revision": "f5acea20083d35f3b510",
+    "url": "https://monterrosa0721.github.io/csb-9q309/static/js/main.9deb1af4.chunk.js"
+  },
+  {
+    "revision": "06b89827b4baefbd4758",
+    "url": "https://monterrosa0721.github.io/csb-9q309/static/js/runtime-main.b8663c99.js"
+  }
+]);
